acb9c14623fbae8073d18cccf5809d9d72b07a43
teffi <steffi.tan07@gmail.com>
2017-07-04 15:06:13 +0800

    Set viewPager to Tablayout.

--- Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -17,6 +17,7 @@ package com.example.android.miwok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -33,7 +34,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ViewPager viewPager = (ViewPager)findViewById(R.id.view_pager);
-        viewPager.setAdapter(new CategoryPageAdapter(getSupportFragmentManager()));
+        //Create categoryPageAdapter and then apply it to viewPager
+        viewPager.setAdapter(new CategoryPageAdapter(this, getSupportFragmentManager()));
+
+        //Find the tab layout that shows the tabs.
+        //Layout xml must have a tab layout design widget.
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+
+        //Connect the tab layout with the view pager. This will
+        // 1. Update the tab layout when the view pager is swiped
+        // 2. Update the view pager when a tab is selected
+        // 3. Set the tab layout's tab names with the view pager's adapter's titles by calling onPageTitle()
+
+        tabLayout.setupWithViewPager(viewPager);
 
 
         /*
