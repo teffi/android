928d31057d74b6027b56e7aea2b455e52a547060
teffi <steffi.tan07@gmail.com>
2017-07-05 18:13:05 +0800

    Added preference change listener in Settings Activity

--- QuakeReport/app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,14 +1,15 @@
 package com.example.android.quakereport;
 
-import android.content.Loader;
+import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
-import android.support.v4.app.LoaderManager;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.util.List;
+import java.util.prefs.PreferenceChangeListener;
 
 public class SettingsActivity extends AppCompatActivity {
     private static final String LOG_TAG = "Settings";
@@ -19,12 +20,37 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
 
-    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener{
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
 
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+
+        }
+
+        /*
+        To do this, our PreferenceFragment can implement the OnPreferenceChangeListener interface to get notified when a preference changes.
+        Then when a single Preference has been changed by the user and is about to be saved, the onPreferenceChange() method
+        will be invoked with the key of the preference that was changed.
+        Note that this method returns a boolean, which allows us to prevent a proposed preference change by returning false.
+        */
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            //Sets the string to appear below Minimum Magnitude
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
     @Override
